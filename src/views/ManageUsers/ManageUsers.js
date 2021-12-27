e44f12a5ebff832398130577f7e643b2ec4ebe8a
John Constantine <warrenching33@gmail.com>
2021-12-27 19:58:57 +0800

    inayus ang roles

--- src/views/ManageUsers/ManageUsers.js
+++ src/views/ManageUsers/ManageUsers.js
@@ -9,6 +9,15 @@ import {
   CCardFooter,
   CCardHeader,
   CCol,
+  CDropdown,
+  CDropdownItem,
+  CDropdownMenu,
+  CDropdownToggle,
+  CModal,
+  CModalBody,
+  CModalFooter,
+  CModalHeader,
+  CModalTitle,
   CProgress,
   CRow,
   CTable,
@@ -45,209 +54,154 @@ import {
 } from '@coreui/icons'
 
 import avatar1 from 'src/assets/images/avatars/1.jpg'
-import avatar2 from 'src/assets/images/avatars/2.jpg'
-import avatar3 from 'src/assets/images/avatars/3.jpg'
-import avatar4 from 'src/assets/images/avatars/4.jpg'
-import avatar5 from 'src/assets/images/avatars/5.jpg'
-import avatar6 from 'src/assets/images/avatars/6.jpg'
-import { cilList, cilShieldAlt, cilMinus,cilPenAlt,cilWeightlifitng } from '@coreui/icons';
+import { cilList, cilShieldAlt, cilMinus, cilPenAlt, cilWeightlifitng } from '@coreui/icons'
 
 import { Link } from 'react-router-dom'
-import { db, functions } from "./../../Firebase";
+import { db, functions } from './../../Firebase'
 // import addAdminRole from "./../../addFunctions"
 
-import grantModeratorRole from "./../../addFunctions" 
-
-const WidgetsDropdown = lazy(() => import('../widgets/WidgetsDropdown.js'))
-const WidgetsBrand = lazy(() => import('../widgets/WidgetsBrand.js'))
+import grantModeratorRole from './../../addFunctions'
 
 import { AuthContext } from '../../contexts/AuthContext'
-
-
 
 const ManageUsers = () => {
   const random = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
   }
-
-  const progressExample = [
-    { title: 'Visits', value: '29.703 Users', percent: 40, color: 'success' },
-    { title: 'Unique', value: '24.093 Users', percent: 20, color: 'info' },
-    { title: 'Pageviews', value: '78.706 Views', percent: 60, color: 'warning' },
-    { title: 'New Users', value: '22.123 Users', percent: 80, color: 'danger' },
-    { title: 'Bounce Rate', value: 'Average Rate', percent: 40.15, color: 'primary' },
-  ]
-
-  const progressGroupExample1 = [
-    { title: 'Monday', value1: 34, value2: 78 },
-    { title: 'Tuesday', value1: 56, value2: 94 },
-    { title: 'Wednesday', value1: 12, value2: 67 },
-    { title: 'Thursday', value1: 43, value2: 91 },
-    { title: 'Friday', value1: 22, value2: 73 },
-    { title: 'Saturday', value1: 53, value2: 82 },
-    { title: 'Sunday', value1: 9, value2: 69 },
-  ]
-
-  const progressGroupExample2 = [
-    { title: 'Male', icon: cilUser, value: 53 },
-    { title: 'Female', icon: cilUserFemale, value: 43 },
-  ]
-
-  const progressGroupExample3 = [
-    { title: 'Organic Search', icon: cibGoogle, percent: 56, value: '191,235' },
-    { title: 'Facebook', icon: cibFacebook, percent: 15, value: '51,223' },
-    { title: 'Twitter', icon: cibTwitter, percent: 11, value: '37,564' },
-    { title: 'LinkedIn', icon: cibLinkedin, percent: 8, value: '27,319' },
-  ]
-
-  const tableExample = [
-    {
-      avatar: { src: avatar1, status: 'success' },
-      user: {
-        name: 'Yiorgos Avraamu',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'USA', flag: cifUs },
-      usage: {
-        value: 50,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'success',
-      },
-      payment: { name: 'Mastercard', icon: cibCcMastercard },
-      activity: '10 sec ago',
-    },
-    {
-      avatar: { src: avatar2, status: 'danger' },
-      user: {
-        name: 'Avram Tarasios',
-        new: false,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Brazil', flag: cifBr },
-      usage: {
-        value: 22,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'info',
-      },
-      payment: { name: 'Visa', icon: cibCcVisa },
-      activity: '5 minutes ago',
-    },
-    {
-      avatar: { src: avatar3, status: 'warning' },
-      user: { name: 'Quintin Ed', new: true, registered: 'Jan 1, 2021' },
-      country: { name: 'India', flag: cifIn },
-      usage: {
-        value: 74,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'warning',
-      },
-      payment: { name: 'Stripe', icon: cibCcStripe },
-      activity: '1 hour ago',
-    },
-    {
-      avatar: { src: avatar4, status: 'secondary' },
-      user: { name: 'Enéas Kwadwo', new: true, registered: 'Jan 1, 2021' },
-      country: { name: 'France', flag: cifFr },
-      usage: {
-        value: 98,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'danger',
-      },
-      payment: { name: 'PayPal', icon: cibCcPaypal },
-      activity: 'Last month',
-    },
-    {
-      avatar: { src: avatar5, status: 'success' },
-      user: {
-        name: 'Agapetus Tadeáš',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Spain', flag: cifEs },
-      usage: {
-        value: 22,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'primary',
-      },
-      payment: { name: 'Google Wallet', icon: cibCcApplePay },
-      activity: 'Last week',
-    },
-    {
-      avatar: { src: avatar6, status: 'danger' },
-      user: {
-        name: 'Friderik Dávid',
-        new: true,
-        registered: 'Jan 1, 2021',
-      },
-      country: { name: 'Poland', flag: cifPl },
-      usage: {
-        value: 43,
-        period: 'Jun 11, 2021 - Jul 10, 2021',
-        color: 'success',
-      },
-      payment: { name: 'Amex', icon: cibCcAmex },
-      activity: 'Last week',
-    },
-  ]
   const { currentUser, currentUserUid } = useContext(AuthContext)
+  const [people, setPeople] = useState([])
+  const [visible, setVisible] = useState(false)
+  const [deleteUser, setDeleteUser] = useState(false)
+  const [unit, setUnit] = useState('newUser')
+  const [admin, setAdmin] = useState(false)
 
-const selectAdmin = () => {
-  const addEmail = "biim@gmail.com"
-  const AddAdmin =  functions.httpsCallable('addAdminRole');
- AddAdmin({email: addEmail}).then(result=> {
-    console.log(result);
-  })
-}
+  const [user, setUser] = useState(null)
+  const [uid, setUId] = useState(null)
+  const selectAdmin = () => {
+    const addEmail = 'biim@gmail.com'
+    const AddAdmin = functions.httpsCallable('addAdminRole')
+    AddAdmin({ email: addEmail }).then((result) => {
+      console.log(result)
+    })
+  }
 
-const pindotUser = () => {
-  
-  const Listahan =  functions.httpsCallable('listUsers');
- Listahan().then(result=> {
-    console.log(result);
-  })
-}
+  const pindotUser = () => {
+    const Listahan = functions.httpsCallable('listUsers')
+    Listahan().then((result) => {
+      console.log(result)
+    })
+  }
 
-const [people, setPeople] =useState([])
-
-useEffect( () => {
-  const unsubscribe = db.collection("users").onSnapshot((snapshot) =>
-  setPeople(
-      snapshot.docs.map((doc) => ({
-        id: doc.id,
-        data: doc.data(),
-      }))
+  const changeUserRole = () => {
+    const AddUnit = functions.httpsCallable('addUserRole')
+    AddUnit({ email: user, unit: unit }).then((result) => {
+      console.log('result', result)
+    })
+    db.collection('users').doc(uid).update({
+      unit: unit,
+    })
+    setVisible(false)
+  }
+  const addAdminRole = () => {
+    const AddUnit = functions.httpsCallable('addAdminRole')
+    AddUnit({ email: user}).then((result) => {
+      console.log('result', result)
+    })
+    db.collection('users').doc(uid).update({
+      admin:true
+    })
+    setVisible(false)
+  }
+  useEffect(() => {
+    const unsubscribe = db.collection('users').onSnapshot((snapshot) =>
+      setPeople(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        })),
+      ),
     )
-  );
-      
-  
-}, []);
+  }, [])
 
-console.log(currentUser)
-console.log("data", people)
+  console.log(currentUser)
+  console.log('data', people)
   return (
     <>
-      <WidgetsDropdown />
-      <CButton 
-      color="secondary"
-      onClick={()=> {
-        selectAdmin()
-      }}
-      size="lg"
-      >Click War</CButton>
- <CButton 
-      color="secondary"
-      onClick={()=> {
-        selectAdmin()
-      }}
-      size="lg"
-      >Tetster</CButton>
       <CRow>
+        {/* Modal */}
+        <CModal alignment="center" visible={visible} onClose={() => setVisible(false)}>
+          <CModalHeader>
+            <CModalTitle>Edit Roles</CModalTitle>
+          </CModalHeader>
+          <CModalBody>
+            <h5>Change User Unit Assignment</h5>
+            <p>
+              Choose the Unit..... {'  '}
+              <CDropdown>
+                <CDropdownToggle color="secondary">{unit} Unit</CDropdownToggle>
+                <CDropdownMenu>
+                  <CDropdownItem type="text" id="unit" onClick={() => setUnit('CPOT')} href="#">
+                    CPOT
+                  </CDropdownItem>
+                  <CDropdownItem type="text" id="unit" onClick={() => setUnit('Charlie')} href="#">
+                    Charlie Company
+                  </CDropdownItem>
+                  <CDropdownItem type="text" id="unit" onClick={() => setUnit('Officer')} href="#">
+                    Others
+                  </CDropdownItem>
+                </CDropdownMenu>
+              </CDropdown>
+            </p>
+            {/* <hr />
+            <h5>Change Roles ADMIN</h5>
+            <p>
+              Change User Role..... {'  '}
+              <CDropdown>
+                <CDropdownToggle color="secondary">
+                  {admin ? 'Admin' : 'Normal User'}
+                </CDropdownToggle>
+                <CDropdownMenu>
+                  <CDropdownItem type="text" id="unit" onClick={() => setAdmin(true)} href="#">
+                    ADMIN
+                  </CDropdownItem>
+                  <CDropdownItem type="text" id="unit" onClick={() => setAdmin(false)} href="#">
+                    Normal User
+                  </CDropdownItem>
+                </CDropdownMenu>
+              </CDropdown>
+            </p> */}
+          </CModalBody>
+          <CModalFooter>
+            <CButton color="secondary" onClick={() => setVisible(false)}>
+              Close
+            </CButton>
+            <CButton onClick={changeUserRole} color="primary">
+              Save changes
+            </CButton>
+          </CModalFooter>
+        </CModal>
+        {/* Modal */}
+
+        {/* Delete Modal */}
+        <CModal alignment="center" visible={deleteUser} onClose={() => setDeleteUser(false)}>
+          <CModalHeader>
+            <CModalTitle>Edit Roles</CModalTitle>
+          </CModalHeader>
+          <CModalBody>
+            <h5>Are you sure to Delete {user}</h5>
+          </CModalBody>
+          <CModalFooter>
+            <CButton color="secondary" onClick={() => setDeleteUser(false)}>
+              Cancel
+            </CButton>
+            <CButton color="primary">Are you sure Deleting this {user}</CButton>
+          </CModalFooter>
+        </CModal>
+        {/* Delete */}
         <CCol xs>
           <CCard className="mb-4">
             <CCardHeader>Traffic {' & '} Sales</CCardHeader>
             <CCardBody>
-
               <br />
 
               <CTable align="middle" className="mb-0 border" hover responsive>
@@ -259,11 +213,10 @@ console.log("data", people)
                     <CTableHeaderCell>Email</CTableHeaderCell>
                     <CTableHeaderCell className="text-center">UID</CTableHeaderCell>
                     <CTableHeaderCell className="text-center">Unit</CTableHeaderCell>
-                    
+
                     <CTableHeaderCell className="text-center">Admin</CTableHeaderCell>
                     <CTableHeaderCell className="text-center">Edit</CTableHeaderCell>
                     <CTableHeaderCell className="text-center">Delete</CTableHeaderCell>
-    
                   </CTableRow>
                 </CTableHead>
                 <CTableBody>
@@ -274,88 +227,51 @@ console.log("data", people)
                       </CTableDataCell>
                       <CTableDataCell>
                         <div>{item.data.name}</div>
-                        <div className="small text-medium-emphasis">
-                         { item.data.email}
-                        </div>
+                        <div className="small text-medium-emphasis">{item.data.email}</div>
                       </CTableDataCell>
 
                       <CTableDataCell className="text-center">
-                     
-                     <div>{item.id}</div>
-                   
-                     </CTableDataCell>
-
-                      <CTableDataCell className="text-center">
-                     
-                      <div>{item.data.unit}</div>
-                    
-                      </CTableDataCell>
-                     
-                      <CTableDataCell className="text-center">
-                        <div>{item.data.admin ? "true": "false"}</div>
+                        <div>{item.id}</div>
                       </CTableDataCell>
 
                       <CTableDataCell className="text-center">
-                        <Link to={"/register"}>
-                        <CIcon size="xl" icon={cilPenAlt} title={"USA"} />
-                        </Link>
+                        <div>{item.data.unit}</div>
                       </CTableDataCell>
 
                       <CTableDataCell className="text-center">
-                        <Link to={"/register"}>
-                        <CIcon size="xl" icon={cilMinus} title={"USA"} />
-                        </Link>
+                        <div>{item.data.admin ? 'true' : 'false'}</div>
                       </CTableDataCell>
-                      
+
+                      <CTableDataCell className="text-center">
+                        <CButton
+                          variant="ghost"
+                          onClick={() => {
+                            setUser(item.data.email)
+                            setUId(item.data.uid)
+                            setVisible(true)
+                          }}
+                        >
+                          <CIcon size="xl" icon={cilPenAlt} title={'USA'} />
+                        </CButton>
+                      </CTableDataCell>
+
+                      <CTableDataCell className="text-center">
+                        <CButton
+                          variant="ghost"
+                          onClick={() => {
+                            setUserId(item.data.uid)
+                            setDeleteUser(true)
+                          }}
+                        >
+                          <CIcon size="xl" icon={cilMinus} title={'USA'} />
+                        </CButton>
+                      </CTableDataCell>
                     </CTableRow>
                   ))}
                 </CTableBody>
               </CTable>
 
               <br />
-              <br />  
-              
-              <CTable align="middle" className="mb-0 border" hover responsive>
-                <CTableHead color="light">
-                  <CTableRow>
-                    <CTableHeaderCell className="text-center">
-                      <CIcon icon={cilPeople} />
-                    </CTableHeaderCell>
-                    <CTableHeaderCell>New User</CTableHeaderCell>
-                    <CTableHeaderCell className="text-center">Country</CTableHeaderCell>
-                    
-                    <CTableHeaderCell className="text-center">Payment Method</CTableHeaderCell>
-    
-                  </CTableRow>
-                </CTableHead>
-                <CTableBody>
-                  {people.map((item, index) => (
-                    <CTableRow v-for="item in tableItems" key={index}>
-                      <CTableDataCell className="text-center">
-                        <CAvatar size="md" src={avatar1} status={'success'} />
-                      </CTableDataCell>
-                      <CTableDataCell>
-                        <div>{item.data.name}</div>
-                        <div className="small text-medium-emphasis">
-                          <span>{true ? 'New' : 'Recurring'}</span> | Registered:{' '}
-                          {"02 Nov 21"}
-                        </div>
-                      </CTableDataCell>
-                      <CTableDataCell className="text-center">
-                        <Link to={"/register"}>
-                        <CIcon size="xl" icon={cifUs} title={"USA"} />
-                        </Link>
-                      </CTableDataCell>
-                     
-                      <CTableDataCell className="text-center">
-                        <CIcon size="xl" icon={cibCcAmex} />
-                      </CTableDataCell>
-                      
-                    </CTableRow>
-                  ))}
-                </CTableBody>
-              </CTable>
-
             </CCardBody>
           </CCard>
         </CCol>
